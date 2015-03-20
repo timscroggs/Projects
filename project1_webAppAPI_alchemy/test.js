29007e52f163154a040a79a11b067a8ff78aad71
tscroggs <tscroggs@gmail.com>
2015-03-20 01:51:54 -0400

    add new alchemy files with further progress

--- project1_webAppAPI_alchemy/test.js
+++ project1_webAppAPI_alchemy/test.js
@@ -1,8 +1,59 @@
-var AlchemyAPI = require('./alchemyapi');
-var alchemyapi = new AlchemyAPI();
+(function () {
+    "use strict";
+
+    var express = require("express"),
+        http = require("http"),
+        app = express();
+    
+    var server = require('http').createServer(app);
+    
+    var AlchemyAPI = require('./alchemyapi');
+    var alchemyapi = new AlchemyAPI();
+
+    //app.use(express.static(__dirname + "/test"));
+    //app.use(express.static(__dirname));
+    //http.createServer(app).listen(3000);
+    
+    
+    var port = process.env.PORT || 3000;
+    server.listen(port, function(){
+        console.log('Express server listening on port ' + port);
+        console.log('To view the example, point your favorite browser to: localhost:3000'); 
+    });
+    
+
+    var myText = "Whoa, AlchemyAPI's Node.js SDK is really great, I can't wait to build my app!";
+
+    
+    
+    //***********************
+    
+    //app.get('/', main);
+    
+    var main = function () {  
+        alchemyapi.sentiment("text", myText, {}, function (response) {
+            console.log("Sentiment: " + response["docSentiment"]["type"]);
+        });
+    };
+
+}());
 
 
-var myText = "Whoa, AlchemyAPI's Node.js SDK is really great, I can't wait to build my app!";
-alchemyapi.sentiment("text", myText, {}, function(response) {
-    console.log("Sentiment: " + response["docSentiment"]["type"]);
-});
+
+
+
+
+
+
+
+
+
+//var consolidate = require('consolidate');
+
+
+//var server = require('http').createServer(app);
+
+//Create the AlchemyAPI object
+
+
+
--- project1_webAppAPI_alchemy/tests.js
+++ project1_webAppAPI_alchemy/tests.js
@@ -1,4 +1,5 @@
-/**
+/*
+*
    Copyright 2014 AlchemyAPI
 
    Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +13,7 @@
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
-*/
+
 
 
 var AlchemyAPI = require('./alchemyapi');
@@ -396,3 +397,4 @@ function combined() {
 	});
 }
 
+*/
