0974ffccd98d1860c44370a664301294ccbb69d8
Tim Scroggs <tscroggs@gmail.com>
2015-03-20 17:48:59 -0400

    add working test.js file for API app

--- project1_webAppAPI_alchemy/test.js
+++ project1_webAppAPI_alchemy/test.js
@@ -1,59 +1,33 @@
-(function () {
-    "use strict";
+var AlchemyAPI = require('./alchemyapi');
+var alchemyapi = new AlchemyAPI();
 
-    var express = require("express"),
-        http = require("http"),
-        app = express();
-    
-    var server = require('http').createServer(app);
-    
-    var AlchemyAPI = require('./alchemyapi');
-    var alchemyapi = new AlchemyAPI();
 
-    //app.use(express.static(__dirname + "/test"));
-    //app.use(express.static(__dirname));
-    //http.createServer(app).listen(3000);
-    
-    
-    var port = process.env.PORT || 3000;
-    server.listen(port, function(){
-        console.log('Express server listening on port ' + port);
-        console.log('To view the example, point your favorite browser to: localhost:3000'); 
+
+var myText = "You arrive at Starbucks. You stare at the line and think to yourself, 'This is going to take forever!' The girl at the front of the line seems to be placing a long order with customized details. You think again, 'Ugh, why are people so picky?'";
+
+
+var demo_url = 'http://www.rantchic.com/2015/01/22/20-things-women-need-to-stop-doing-at-starbucks/?utm_source=RantLifestyle&utm_medium=RantWidget&utm_term=Slot10';
+
+var demo_html = '<html><head><title>Node.js Demo | AlchemyAPI</title></head><body><h1>Did you know that AlchemyAPI works on HTML?</h1><p>Well, you do now.</p></body></html>';
+
+//***********************
+
+function myFunction() {
+    alchemyapi.sentiment("text", myText, {}, function (response) {
+        console.log("Sentiment 1: " + response["docSentiment"]["type"]);
     });
-    
-
-    var myText = "Whoa, AlchemyAPI's Node.js SDK is really great, I can't wait to build my app!";
-
-    
-    
-    //***********************
-    
-    //app.get('/', main);
-    
-    var main = function () {  
-        alchemyapi.sentiment("text", myText, {}, function (response) {
-            console.log("Sentiment: " + response["docSentiment"]["type"]);
-        });
-    };
-
-}());
+}
 
 
+alchemyapi.sentiment("text", myText, {}, function (response) {
+    var $result = "Sentiment 1: " + response["docSentiment"]["type"];
+    console.log($result);
+});
 
+alchemyapi.sentiment("url", demo_url, {}, function (response) {
+    console.log("Sentiment 2: " + response["docSentiment"]["type"]);
+});
 
-
-
-
-
-
-
-
-//var consolidate = require('consolidate');
-
-
-//var server = require('http').createServer(app);
-
-//Create the AlchemyAPI object
-
-
-
+alchemyapi.sentiment("html", demo_html, {}, function (response) {
+    console.log("Sentiment 3: " + response["docSentiment"]["type"]);
+});
